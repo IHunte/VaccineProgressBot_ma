f012c9be6a81273f86dc28e75a783a3db238a2dc
IHunte <IHunte.Dev@gmail.com>
2021-06-01 17:21:44 +0100

    add route to use cron-job

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,9 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\VaccinationState;
+
+Route::get('/execute_bot', function () {
+    Artisan::call('VaccinationStateBot:start');
+});
