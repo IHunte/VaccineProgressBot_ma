e4c3cd23eb8dd2b611c184d50143533dacd8d6c3
IHunte <IHunte.Dev@gmail.com>
2021-06-01 13:53:19 +0100

    add VaccinationState Command

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'VACCIN_OWID_API' => 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json',
+    'COUNTRY' => 'Morocco',
+    'TARGET_POPULATION' => 30_000_000,
+    'EMOTE' => [
+        'SYRINGE' => '💉',
+    ]
+];
